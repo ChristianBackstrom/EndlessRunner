4e2b374fc2fda605698453a7d067e9c75482ae0d
ChristianBackstrom <christian.berggren.backstrom@gmail.com>
2023-04-11 16:26:10 +0200

    Movement working, switch between lanes

--- Source/EndlessRunner/MovementController.cpp
+++ Source/EndlessRunner/MovementController.cpp
@@ -2,27 +2,29 @@
 
 
 #include "MovementController.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInput/Public/InputMappingContext.h"
+#include "EnhancedInput/Public/EnhancedInputSubsystems.h"
 
 // Sets default values
 AMovementController::AMovementController()
 {
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-
 }
 
 // Called when the game starts or when spawned
 void AMovementController::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
+	Currentlane = 0;
 }
 
 // Called every frame
 void AMovementController::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-
 }
 
 // Called to bind functionality to input
@@ -30,5 +32,33 @@ void AMovementController::SetupPlayerInputComponent(UInputComponent* PlayerInput
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+	APlayerController* PC = Cast<APlayerController>(GetController());
+	
+	UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PC->GetLocalPlayer());
+	
+	// Clear out existing mapping, and add our mapping
+	Subsystem->ClearAllMappings();
+	Subsystem->AddMappingContext(DefaultMappingContext, 0);
+
+	// Set up action bindings
+	if (UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(PlayerInputComponent))
+	{
+		EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Started, this, &AMovementController::Move);
+	}
 }
+
+void AMovementController::Move(const FInputActionValue& Value)
+{
+	Currentlane += Value.Get<float>();
+
+	Currentlane = FMath::Clamp(Currentlane,0, Lanes.Num() - 1);
+
+	MoveToLane(Currentlane);
+}
+
+void AMovementController::MoveToLane(int32 index)
+{
+	SetActorLocation(Lanes[index]);
+}
+
 
--- Source/EndlessRunner/MovementController.h
+++ Source/EndlessRunner/MovementController.h
@@ -4,26 +4,48 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
+#include "InputActionValue.h"
+#include "InputMappingContext.h"
 #include "MovementController.generated.h"
 
 UCLASS()
-class ENDLESSRUNNER_API AMovementController : public ACharacter
+class ENDLESSRUNNER_API AMovementController : public APawn
 {
 	GENERATED_BODY()
+
+protected:
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	class UInputMappingContext* DefaultMappingContext;
+
+	/** Jump Input Action */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	class UInputAction* MoveAction;
 
 public:
 	// Sets default values for this character's properties
 	AMovementController();
 
-protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
-
-public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+private:
+	void Move(const FInputActionValue& Value);
+
+public:
+	void MoveToLane(int32 index);
+	
+	UPROPERTY(VisibleAnywhere)
+	TArray<FVector> Lanes;
+
+private:
+	int32 Currentlane;
 
 };
